39c02851b8ad208648d8b04152621a47e05f6cbb
sohee <sweetbox95@naver.com>
2024-12-19 23:05:29 +0900

    css 경로 수정

--- common/js/include.js
+++ common/js/include.js
@@ -2,8 +2,8 @@ document.write(`
     <script src="https://unpkg.com/gsap@3/dist/gsap.min.js"></script>
     <script src="https://unpkg.com/gsap@3/dist/ScrollTrigger.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="/common/plugins/fontawesome-free-6.1.2-web/js/all.js"></script>
+    <script src="./common/plugins/fontawesome-free-6.1.2-web/js/all.js"></script>
     
-    <link href="/common/css/app.css" rel="stylesheet"/>
-    <script src="/common/js/common.js"></script>
+    <link href="./common/css/app.css" rel="stylesheet"/>
+    <script src="./common/js/common.js"></script>
 `)
